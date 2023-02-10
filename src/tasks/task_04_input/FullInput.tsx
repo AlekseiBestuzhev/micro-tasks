10dd05a30cc568f50ee56668e3fdff78edb90551
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-02-10 21:48:51 +0400

    feat: task_04 realize adding text to list by myself

---
+++ src/tasks/task_04_input/FullInput.tsx
@@ -0,0 +1,19 @@
+import React, { ChangeEvent, FC, useState } from 'react';
+
+type FullInputType = {
+	addMessage: (title: string) => void
+}
+
+export const FullInput: FC<FullInputType> = (props): JSX.Element => {
+	const [title, setTitle] = useState('');
+	const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+		setTitle(event.currentTarget.value);
+	}
+	const onClickButtonHendler = () => props.addMessage(title);
+	return (
+		<div>
+			<input onChange={onChangeInputHandler} />
+			<button onClick={onClickButtonHendler}>+</button>
+		</div>
+	);
+}
--- src/tasks/task_04_input/TaskInput.tsx
+++ src/tasks/task_04_input/TaskInput.tsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
+import { FullInput } from './FullInput';
 
 export const TaskInput = () => {
 	const [message, setMessage] = useState([
 		{ message: 'message 1' },
 		{ message: 'message 2' },
 		{ message: 'message 3' },
-	])
+	]);
+	const addMessage = (title: string) => {
+		const newMessage = [...message, { message: title }];
+		setMessage(newMessage);
+	}
 
 	return (
 		<div>
 			<h2 className='task'>Task 04. Input</h2>
-			<div>
-				<input type="text" />
-				<button>+</button>
-			</div>
+			<FullInput addMessage={addMessage} />
 			{message.map((elem, index) => {
 				return (
 					<div key={index}>{elem.message}</div>
